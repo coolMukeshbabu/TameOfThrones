dc4029c865a461583d027c4404d22a5c80619120
coolMukeshbabu <82643895+coolMukeshbabu@users.noreply.github.com>
2021-04-19 20:09:28 +0530

    Add files via upload

---
+++ TameOfThrones/TameOfThrones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TameOfThrones.Services;
+
+namespace TameOfThrones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<Kingdom> kingdoms = Helper.GetKingdoms();
+            Print(kingdoms);
+
+            //Read secret messages
+
+            
+        }
+
+        public static void Print(List<Kingdom> kingdoms)
+        {
+            foreach(var kingdom in kingdoms)
+            {
+                Console.WriteLine(kingdom.GetName());
+            }
+            Console.ReadLine();
+        }
+    }
+}
