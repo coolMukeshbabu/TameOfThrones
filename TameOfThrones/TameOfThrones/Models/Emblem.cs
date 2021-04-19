38097539dd88beb1f595ef261f53c8c1c16f3b99
coolMukeshbabu <82643895+coolMukeshbabu@users.noreply.github.com>
2021-04-19 20:08:08 +0530

    Added files

---
+++ TameOfThrones/TameOfThrones/Models/Emblem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TameOfThrones
+{
+    public class Emblem
+    {
+        private string name;
+        public Emblem(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ TameOfThrones/TameOfThrones/Models/Kingdom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TameOfThrones
+{
+    public class Kingdom
+    {
+        private Emblem emblem { get; set; }
+        private string name { get; set; }
+        public Kingdom(string name,Emblem emblem)
+        {
+            this.name = name;
+            this.emblem = emblem;
+        }
+
+        public Emblem GetEmblem()
+        {
+            return emblem;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+
+        
+
+    }
+}
